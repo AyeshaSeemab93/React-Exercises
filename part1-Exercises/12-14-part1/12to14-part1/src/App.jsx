cd70ad3975fbcbbe20e2baa8d680bda18e96ab43
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-10-06 13:11:55 +0300

    anecdotes step 3 Ex 14

--- part1-Exercises/12-14-part1/12to14-part1/src/App.jsx
+++ part1-Exercises/12-14-part1/12to14-part1/src/App.jsx
@@ -13,32 +13,78 @@ const App = () => {
   ]
    
   const [selected, setSelected] = useState(0)
-    const selectRandom = ()=>{
+  // create a zero-filled array
+  const[voteArray, setVoteArray] = useState(new Uint16Array(anecdotes.length))
+
+  const selectRandom = ()=>{
       //generating a random number (Math.floor()- to get integer ,not decimal)
        let x = Math.floor(Math.random() * anecdotes.length);
       console.log("x", x);
       //making selected == x
       setSelected(x);
       console.log("selected", selected);
-   
-    }
+      
+  }
+
+  const voteIt = () =>{
+    
+    console.log("voteArray: ",voteArray);
+    console.log("current location in anecdotes: ", selected);
+    // Create a copy of the voteArray( don't directly modify the state)
+    const updatedVoteArray = [...voteArray];
+    updatedVoteArray[selected] += 1;
+    setVoteArray(updatedVoteArray);
+    //call the method to check and then post the most voted acedote everytime on voting.
+    MostVotes(updatedVoteArray, anecdotes);
+  }
   return (
-    <>
-     {/* <div>{anecdotes[selected]}</div> */}
-     <Display anecdotes = {anecdotes} selected = {selected}/> 
-    <button onClick={selectRandom}>next anecdote</button>
-    </>
+    <div>
+    <Header text = " Anecdote of the day"/>
+    <Display anecdotes = {anecdotes} selected = {selected}/> 
+    <div>has {voteArray[selected]} votes</div>
+    <Button handleclick={voteIt} text= "Vote"/>
+    <Button handleclick={selectRandom} text= "next anecdote" />
+    <Header text = " Anecdote with most votes"/>
+    <MostVotes voteArray={voteArray} anecdotes={anecdotes} />
+
+    </div>
   )
 }
 const Display = ({anecdotes, selected}) =>{
-  console.log(anecdotes, selected);
+  //console.log(anecdotes, selected);
 return(
   <div>
-    <div>
-      {anecdotes[selected]}
-    </div>
+    <div>{anecdotes[selected]}</div>
     </div>
 )
 }
+const Button = ({handleclick, text}) => <button onClick={handleclick}>{text}</button>
+  
+
+const Header = ({text}) =>  <h1>{text}</h1>
+
+
+const MostVotes = ({ voteArray, anecdotes }) => {
+  // Check if voteArray or anecdotes is undefined
+  if (!voteArray || !anecdotes) {
+    return null; // or return a loading message or some fallback content
+  }
+
+  let maxVotes = 0;
+  let maxIndex = 0;
+
+  for (let i = 0; i < voteArray.length; i++) {
+    if (voteArray[i] > maxVotes) {
+      maxVotes = voteArray[i];
+      maxIndex = i;
+    }
+  }
+  return (
+    <div>
+      <Display anecdotes={anecdotes} selected={maxIndex} />
+      <div>has {maxVotes} votes</div>
+    </div>
+  );
+};
 
 export default App
