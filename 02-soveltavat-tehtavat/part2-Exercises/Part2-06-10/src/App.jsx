93fd3fd82fdd006472815bda1a32f815c55b86ce
AyeshaSeemab93 <ayeshaseemab2017@gmail.com>
2023-10-26 02:16:03 +0300

    2.9*: The Phonebook Step4

--- 02-soveltavat-tehtavat/part2-Exercises/Part2-06-10/src/App.jsx
+++ 02-soveltavat-tehtavat/part2-Exercises/Part2-06-10/src/App.jsx
@@ -9,14 +9,31 @@ function App(){
                                         ])
   const [newName, setNewName] = useState('abc')
   const[newNum, setNewNum] = useState('123')
+  const[searchedName, setSearchedName] = useState('search')
+  const[showAll, setShowAll]= useState(true)
+
+  //IF showAll is true then show full persons list else filter to find the searched name
+  var phonebookToShow= showAll
+  ? persons
+  :(persons.filter(person => 
+    {
+      const lowercaseName = person.name.toLowerCase();
+      const lowercaseQuery = searchedName.toLowerCase();
+      return lowercaseName.includes(lowercaseQuery); //check if name matches with the searched name(lower+ upper cases both checked here)
+    }
+    ))
 
   function ChnageText(event){
-  console.log('event', event)
-  setNewName(event.target.value)
+    console.log('event', event)
+    setNewName(event.target.value)
   }
   function ChangeNum(event){
-setNewNum(event.target.value)
+    setNewNum(event.target.value)
   }
+  function ChangeSearch(event){
+    setSearchedName(event.target.value)
+  }
+
 
   function AddPerson(){
     // const nameExists = persons.some(person=>person.name === newName);
@@ -41,6 +58,10 @@ setNewNum(event.target.value)
   return(
     <div>
       <h2>Phonebook</h2>
+      filer shown with<input value={searchedName} onChange={ChangeSearch} onClick={() => setShowAll(!showAll)}/>
+      
+
+      <h2>add a new</h2>
       <form onSubmit={function(event){ console.log(event); event.preventDefault()}}>
         <div>
         name: <input type="text" value={newName} onChange={ChnageText}/>
@@ -53,7 +74,8 @@ setNewNum(event.target.value)
         </div>
 
         <h2>Numbers</h2>
-        {persons.map(person => <Person key={person.id} person={person} />)}
+       
+        {phonebookToShow.map(person => <Person key={person.id} person={person} />)}
 
       </form>
     </div>
